8b0abe0c8d4f9dba22339185962805dbfa4a21c9
Helia Gohar <helia77@users.noreply.github.com>
2025-11-17 20:10:32 -0600

    Fixed memory bug to avoid UB

--- source/tvote3/tvote3_hsa.cu
+++ source/tvote3/tvote3_hsa.cu
@@ -24,32 +24,32 @@ float* hsa_eigenvectors3spherical(float* tensors, float* evals, unsigned int n, 
 }
 
 glm::mat3* hsa_gaussian3(const glm::mat3* source, const unsigned int s0, const unsigned int s1, const unsigned int s2, const float sigma, glm::vec3 pixel_size,
-    unsigned int& out_s0, unsigned int& out_s1, unsigned int& out_s2, const int deviceID = 0) {
+    unsigned int& out_s0, unsigned int& out_s1, unsigned int& out_s2, const int device) {
 
 	// CPU implementation
-    if (deviceID < 0) {
+    if (device < 0) {
         unsigned kernel_size0 = (unsigned)(6 * sigma * pixel_size[0]);
         float* gauss0 = tira::cpu::kernel_gaussian<float>(kernel_size0, 0, sigma, pixel_size[0]);
         glm::mat3* dest_0 = tira::cpu::convolve3<glm::mat3, float>(source, s0, s1, s2, gauss0, kernel_size0, 1, 1, out_s0, out_s1, out_s2);
-        free(gauss0);
+        delete[] gauss0;
 
         unsigned kernel_size1 = (unsigned)(6 * sigma * pixel_size[1]);
         float* gauss1 = tira::cpu::kernel_gaussian<float>(kernel_size1, 0, sigma, pixel_size[1]);
         glm::mat3* dest_1 = tira::cpu::convolve3<glm::mat3, float>(dest_0, out_s0, out_s1, out_s2, gauss1, 1, kernel_size1, 1, out_s0, out_s1, out_s2);
-        free(gauss1);
-        free(dest_0);
+        delete[] gauss1;
+        delete[] dest_0;
 
         unsigned kernel_size2 = (unsigned)(6 * sigma * pixel_size[2]);
         float* gauss2 = tira::cpu::kernel_gaussian<float>(kernel_size2, 0, sigma, pixel_size[2]);
         glm::mat3* dest_2 = tira::cpu::convolve3<glm::mat3, float>(dest_1, out_s0, out_s1, out_s2, gauss2, 1, 1, kernel_size2, out_s0, out_s1, out_s2);
-        free(gauss2);
-        free(dest_1);
+        delete[] gauss2;
+        delete[] dest_1;
 
         return dest_2;
     }
 
 	// CUDA implementation
-    cudaSetDevice(deviceID);
+    cudaSetDevice(device);
     glm::mat3* dest = tira::cuda::gaussian_filter3d<glm::mat3>(source, s0, s1, s2, sigma, sigma, sigma, out_s0, out_s1, out_s2);
     return dest;
 }
